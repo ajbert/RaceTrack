93c9e5c3f2feeefea66ddbf16cffedae299968d8
ajbert <100375590+ajbert@users.noreply.github.com>
2022-10-05 19:15:56 -0500

    Add files via upload

---
+++ RaceTrack/Cars/GoKart.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Cars
+{
+    public class GoKart : RaceCar
+    {
+        public GoKart()
+        {
+            Name = "Go Kart";
+            TopSpeed = 85;
+        }
+        public override void StartEngine()
+        {
+            Console.WriteLine($"The {Name} motor is obnoxiously loud");
+        }
+
+        public override void Brake()
+        {
+            Console.WriteLine($"The {Name} runs over a banana peel and spins over the finish line!");
+            base.Brake();
+        }
+    }
+}
