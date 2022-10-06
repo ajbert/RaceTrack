04bf37709cd759273eca62ddcf2d19d9f1598602
ajbert <100375590+ajbert@users.noreply.github.com>
2022-10-05 19:15:27 -0500

    Add files via upload

---
+++ RaceTrack/Drivers/Bowser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RaceTrack.RaceTrack.Cars;
+
+namespace RaceTrack.RaceTrack.Drivers
+{
+    public class Bowser : Driver
+    {
+        public Bowser(RaceCar car) : base(car)
+        {
+            Name = "Bowser";
+            SkillLevel = 11;
+        }
+
+        public override void Drive()
+        {
+         Car.Accelerate(SkillLevel);
+        }
+    }
+}
